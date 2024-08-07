e9b5ef5bf6f66e8789762f112f41d87d25e325f4
Hassan Barati <48393874+hsnpuply@users.noreply.github.com>
2024-08-07 12:56:41 +0330

    Fix Feature products button
    if buttons clicked will get underline and rest of them remove their underline

--- Alukas_Jewelry/src/views/home_pages/home_feature_prodcuts/home_featured_products.vue
+++ Alukas_Jewelry/src/views/home_pages/home_feature_prodcuts/home_featured_products.vue
@@ -2,10 +2,28 @@
   <section class="feature_products_section  py-12">
     <div class="feature_products pt-4 ">
       <h2 class="text-4xl text-black text-center">Feature Products</h2>
-      <div class=" options-button flex items-center gap-6 justify-center py-5 ">
-          <button class=" shop_now hover:text-black duration-100 text-xl text-black border-b-2 border-black">New Arrivals</button>
-          <button class="shop_now hover:text-black duration-100 text-xl text-gray-500">Featured</button>
-          <button class="shop_now hover:text-black duration-100 text-xl text-gray-500">Best Seller</button>
+      <div class=" options-button flex items-center gap-8 justify-center py-5 ">
+ <button
+          :class="{ 'seletcedProduct': selectedButton === 'new-arrivals', 'unSelected-product': selectedButton !== 'new-arrivals' }"
+          @click="selectedButton = 'new-arrivals'"
+          class="hover:text-black duration-100 text-xl text-gray-500"
+        >
+          New Arrivals
+        </button>
+        <button
+          :class="{ 'seletcedProduct': selectedButton === 'featured', 'unSelected-product': selectedButton !== 'featured' }"
+          @click="selectedButton = 'featured'"
+          class="relative hover:text-black duration-100 text-xl text-gray-500"
+        >
+          Featured
+        </button>
+        <button
+          :class="{ 'seletcedProduct': selectedButton === 'best-seller', 'unSelected-product': selectedButton !== 'best-seller' }"
+          @click="selectedButton = 'best-seller'"
+          class="relative hover:text-black duration-100 text-xl text-gray-500"
+        >
+          Best Seller
+        </button>
       </div>
 
 
@@ -18,7 +36,8 @@
     class="cursor-pointer container"
   > 
     <swiper-slide class="releative">
-      <img :src="FeatureProducts.feature_products_items[0].pic" alt="">
+      <img src="../../../assets/featured_products/best_seller/back_product-1.webp" alt="">
+       
     </swiper-slide>
 
 
@@ -68,7 +87,44 @@ fetchingData()
 
 
 
-
+const selectedButton = ref('new-arrivals');
 
 
 </script>
+
+<style scoped>
+
+
+.unSelected-product,.seletcedProduct{
+  position: relative;
+  overflow: hidden;
+}
+.seletcedProduct{
+  color:black
+}
+
+.unSelected-product::after,.seletcedProduct::after{
+  content: '';
+  height: 2px;
+  background-color: rgb(0, 0, 0);
+  position: absolute;
+  bottom: 0;
+  left: 0%;
+  transition: all .5s .2s ease-out, transform 1s .2s ease-out;
+
+}
+.unSelected-product::after{
+  width: 0;
+}
+
+.seletcedProduct::after{
+  width: 100%;
+}
+
+.unSelected-product:hover::after{
+  width: 100%;
+}
+
+
+
+</style>
