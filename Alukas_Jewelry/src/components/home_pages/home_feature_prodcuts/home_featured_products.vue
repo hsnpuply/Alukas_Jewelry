4aa10dd4732db8b2223481a08bffcc6982e9f2c9
Hassan Barati <48393874+hsnpuply@users.noreply.github.com>
2024-09-08 09:31:16 +0330

    Product component
    Products component added to featured Product

--- Alukas_Jewelry/src/components/home_pages/home_feature_prodcuts/home_featured_products.vue
+++ Alukas_Jewelry/src/components/home_pages/home_feature_prodcuts/home_featured_products.vue
@@ -1,31 +1,6 @@
 <template>
-  <section class="feature_products_section   overflow-hidden pb-24 pt-16">
-    <!-- <div class="group/hasan group w-1/3" >
-            <div class="product-frame bg-gray-600 p-3 hover:bg-white    "   >
-        <div class="image-frame "  >
-        <img src="../../../assets/featured_products/best_seller/product-2.webp" alt="" class="group-hover/hasan:opacity-0  duration-700">
-        <img src="../../../assets/featured_products/best_seller/back_product-2.webp" alt="" class=" opacity-0 group-hover/hasan:opacity-100 duration-700 absolute top-0 left-0
-        "
-        >
-        </div>
+  <section class="feature_products_section   overflow-hidden pb-24 pt-16 px-2 lg:px-0">
 
-        <div class="info-product py-4  
-        flex flex-col gap-1 duration-500  " 
-        >
-          <h4 class="product-brand text-sm text-[#757575] " >
-            Amo Hasan
-          </h4>
-          <h3 class="text-lg"> 
-        Cross Stripes & Stone Bracelet
-          </h3>
-
-          <p class="text-lg">$169.00</p>
-          <button class="bg-black text-white p-3 
-          cursor-pointer" >Random add to</button>
-          
-        </div>
-      </div>
-    </div> -->
     <div class="feature_products pt-4 relative">
       <h2 class="text-4xl text-black text-center">Feature Products</h2>
 
@@ -51,301 +26,35 @@
 
 
         <div class="new_arrivals_products flex items-center justify-center
-       bg-violet-7002  absolute w-full top-[7.6rem]"
+       bg-violet-7002  absolute w-full top-[6rem]  py-5 px-8"
           :class="selectedButton === 'new-arrivals' ? 'appearList' : 'disappearList' + ' down'">
-          <swiper :modules="modules" :slides-per-view="4" :space-between="50" class="cursor-pointer container ">
-            <swiper-slide class="releative group/hasan group group">
-              <div class="product_slider group overflow-hidden
-      before:group-hover:opacity-100 before:duration-500 after:group-hover:opacity-100 after:duration-500 
-      h-[460px]
-      ">
-                <div class="img_frame relative h-[340px] overflow-hidden ">
-                  <img src="../../../assets/featured_products/best_seller/product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-100 group-hover:opacity-0
-                duration-500 min-w-[340px] min-h-[340px]">
-                  <img src="../../../assets/featured_products/best_seller/back_product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-0  group-hover:opacity-100
-                min-w-[340px] min-h-[340px] duration-500">
-                  <div
-                    class="search bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-4 translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]">
-                    <Icon icon="mdi-light:magnify" class="text-3xl  " />
-                  </div>
-                  <div
-                    class="wishlist bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-[4.5rem] translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]"
-                    @click="toggleMyHeart()">
-                    <Icon :icon="toggleHeart" class="text-3xl" />
-                  </div>
+          <swiper :modules="modules" :breakpoints="breakpoints" space-between="3%" class="cursor-pointer container   ">
 
-                </div>
-                <div class="info-product pt-4 overflow-hidden max-h-[7rem] group-hover:max-h-[10rem] px-3
-          bg-white flex  flex-col justify-start
-        group-hover:-translate-y-10 duration-500  ">
-                  <h4 class="product-brand text-sm text-[#757575] ">
-                    Hasan barati Maleki dasta bilquid
-                  </h4>
-                  <h3 class="text-lg">
-                    Cross Stripes & Stone Bracelet
-                  </h3>
-
-                  <p class="text-lg">$169.00</p>
-
-
-
-                  <div class="add-to-wishlist opacity-0 group-hover:opacity-100
-                   transition-all duration-500 py-0 translate-y-24
-                   group-hover:translate-y-0 group-hover:py-2
-                   
-                      ">
-                    <button class="bg-black text-white p-3  
-          rounded-sm    ">
-                      Add to my WishList
-                    </button>
-
-                  </div>
-
-
-                </div>
-
-              </div>
-
+            <swiper-slide v-for="(item, index) in 4" :key="index">
+              <Products />
             </swiper-slide>
 
+          </swiper>
+        </div>
 
-            <swiper-slide class="releative group/hasan group">
-              <div class="product_slider group overflow-hidden
-            before:group-hover:opacity-100 before:duration-500 after:group-hover:opacity-100 after:duration-500 duration-300 bg-white
-      h-[460px]
-      ">
-                <div class="img_frame relative h-[340px] overflow-hidden ">
-                  <img src="../../../assets/featured_products/best_seller/product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-100 group-hover:opacity-0
-                duration-500 min-w-[340px] min-h-[340px]">
-                  <img src="../../../assets/featured_products/best_seller/back_product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-0  group-hover:opacity-100
-                min-w-[340px] min-h-[340px] duration-500">
-                  <div
-                    class="search bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-4 translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]">
-                    <Icon icon="mdi-light:magnify" class="text-3xl  " />
-                  </div>
-                  <div
-                    class="wishlist bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-[4.5rem] translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]"
-                    @click="toggleMyHeart()">
-                    <Icon :icon="toggleHeart" class="text-3xl" />
-                  </div>
+        <div class="featured_products flex items-center justify-center
+       absolute w-full top-[7.6rem]  " :class="selectedButton === 'featured' ? 'appearList' : 'disappearList'">
+          <swiper :modules="modules" :breakpoints="breakpoints" space-between="3%" class="cursor-pointer container  ">
 
-                </div>
-                <div class="info-product pt-4 overflow-hidden max-h-[7rem] group-hover:max-h-[10rem] px-3
-          bg-white flex  flex-col justify-start
-        group-hover:-translate-y-10 duration-500  ">
-                  <h4 class="product-brand text-sm text-[#757575] ">
-                    Hasan barati Maleki dasta bilquid
-                  </h4>
-                  <h3 class="text-lg">
-                    Cross Stripes & Stone Bracelet
-                  </h3>
-
-                  <p class="text-lg">$169.00</p>
-
-
-
-                  <div class="add-to-wishlist opacity-0 group-hover:opacity-100
-                   transition-all duration-500 py-0 translate-y-24
-                   group-hover:translate-y-0 group-hover:py-2
-                   
-                      ">
-                    <button class="bg-black text-white p-3  
-          rounded-sm    ">
-                      Add to my WishList
-                    </button>
-
-                  </div>
-
-
-                </div>
-
-              </div>
-
+            <swiper-slide v-for="(item, index) in 5" :key="index">
+              <Products />
             </swiper-slide>
-
-
-
-            <swiper-slide class="releative group/hasan group">
-              <div class="product_slider group overflow-hidden
-            before:group-hover:opacity-100 before:duration-500 after:group-hover:opacity-100 after:duration-500 duration-300 bg-white
-      h-[460px]
-      ">
-                <div class="img_frame relative h-[340px] overflow-hidden ">
-                  <img src="../../../assets/featured_products/best_seller/product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-100 group-hover:opacity-0
-                duration-500 min-w-[340px] min-h-[340px]">
-                  <img src="../../../assets/featured_products/best_seller/back_product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-0  group-hover:opacity-100
-                min-w-[340px] min-h-[340px] duration-500">
-                  <div
-                    class="search bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-4 translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]">
-                    <Icon icon="mdi-light:magnify" class="text-3xl  " />
-                  </div>
-                  <div
-                    class="wishlist bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-[4.5rem] translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]"
-                    @click="toggleMyHeart()">
-                    <Icon :icon="toggleHeart" class="text-3xl" />
-                  </div>
-
-                </div>
-                <div class="info-product pt-4 overflow-hidden max-h-[7rem] group-hover:max-h-[10rem] px-3
-          bg-white flex  flex-col justify-start
-        group-hover:-translate-y-10 duration-500  ">
-                  <h4 class="product-brand text-sm text-[#757575] ">
-                    Hasan barati Maleki dasta bilquid
-                  </h4>
-                  <h3 class="text-lg">
-                    Cross Stripes & Stone Bracelet
-                  </h3>
-
-                  <p class="text-lg">$169.00</p>
-
-
-
-                  <div class="add-to-wishlist opacity-0 group-hover:opacity-100
-                   transition-all duration-500 py-0 translate-y-24
-                   group-hover:translate-y-0 group-hover:py-2
-                   
-                      ">
-                    <button class="bg-black text-white p-3  
-          rounded-sm    ">
-                      Add to my WishList
-                    </button>
-
-                  </div>
-
-
-                </div>
-
-              </div>
-
-            </swiper-slide>
-
-
-
-            <swiper-slide class="releative group/hasan group">
-              <div class="product_slider group overflow-hidden
-            before:group-hover:opacity-100 before:duration-500 after:group-hover:opacity-100 after:duration-500 duration-300 bg-white
-      h-[460px]
-      ">
-                <div class="img_frame relative h-[340px] overflow-hidden ">
-                  <img src="../../../assets/featured_products/best_seller/product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-100 group-hover:opacity-0
-                duration-500 min-w-[340px] min-h-[340px]">
-                  <img src="../../../assets/featured_products/best_seller/back_product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-0  group-hover:opacity-100
-                min-w-[340px] min-h-[340px] duration-500">
-                  <div
-                    class="search bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-4 translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]">
-                    <Icon icon="mdi-light:magnify" class="text-3xl  " />
-                  </div>
-                  <div
-                    class="wishlist bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-[4.5rem] translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]"
-                    @click="toggleMyHeart()">
-                    <Icon :icon="toggleHeart" class="text-3xl" />
-                  </div>
-
-                </div>
-                <div class="info-product pt-4 overflow-hidden max-h-[7rem] group-hover:max-h-[10rem] px-3
-          bg-white flex  flex-col justify-start
-        group-hover:-translate-y-10 duration-500  ">
-                  <h4 class="product-brand text-sm text-[#757575] ">
-                    Hasan barati Maleki dasta bilquid
-                  </h4>
-                  <h3 class="text-lg">
-                    Cross Stripes & Stone Bracelet
-                  </h3>
-
-                  <p class="text-lg">$169.00</p>
-
-
-
-                  <div class="add-to-wishlist opacity-0 group-hover:opacity-100
-                   transition-all duration-500 py-0 translate-y-24
-                   group-hover:translate-y-0 group-hover:py-2
-                   
-                      ">
-                    <button class="bg-black text-white p-3  
-          rounded-sm    ">
-                      Add to my WishList
-                    </button>
-
-                  </div>
-
-
-                </div>
-
-              </div>
-
-            </swiper-slide>
-
 
 
           </swiper>
         </div>
 
-        <div class="new_arrivals_products flex items-center justify-center
-       absolute w-full top-[7.6rem]  " :class="selectedButton === 'featured' ? 'appearList' : 'disappearList'">
-          <swiper :modules="modules" :slides-per-view="4" :space-between="50" class="cursor-pointer container  ">
+        <div class="bestSeller_products flex items-center justify-center 
+       absolute w-full top-[7.6rem]  " :class="selectedButton === 'best-seller' ? 'appearList' : 'disappearList'">
+          <swiper :modules="modules" :breakpoints="breakpoints" space-between="3%" class=" cursor-pointer container  ">
 
-            <swiper-slide class="releative group/hasan group">
-              <div class="product_slider group overflow-hidden
-            before:group-hover:opacity-100 before:duration-500 after:group-hover:opacity-100 after:duration-500 duration-300 bg-white
-      h-[460px]
-      ">
-                <div class="img_frame relative h-[340px] overflow-hidden ">
-                  <img src="../../../assets/featured_products/best_seller/product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-100 group-hover:opacity-0
-                duration-500 min-w-[340px] min-h-[340px]">
-                  <img src="../../../assets/featured_products/best_seller/back_product-1.webp" alt="" class="
-              absolute top-0 left-0 opacity-0  group-hover:opacity-100
-                min-w-[340px] min-h-[340px] duration-500">
-                  <div
-                    class="search bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-4 translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]">
-                    <Icon icon="mdi-light:magnify" class="text-3xl  " />
-                  </div>
-                  <div
-                    class="wishlist bg-gray-400 !bg-white rounded-full p-2 absolute right-3 top-[4.5rem] translate-x-5 opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]"
-                    @click="toggleMyHeart()">
-                    <Icon :icon="toggleHeart" class="text-3xl" />
-                  </div>
-
-                </div>
-                <div class="info-product pt-4 overflow-hidden max-h-[7rem] group-hover:max-h-[10rem] px-3
-          bg-white flex  flex-col justify-start
-        group-hover:-translate-y-10 duration-500  ">
-                  <h4 class="product-brand text-sm text-[#757575] ">
-                    Hasan barati Maleki dasta bilquid
-                  </h4>
-                  <h3 class="text-lg">
-                    Cross Stripes & Stone Bracelet
-                  </h3>
-
-                  <p class="text-lg">$169.00</p>
-
-
-
-                  <div class="add-to-wishlist opacity-0 group-hover:opacity-100
-                   transition-all duration-500 py-0 translate-y-24
-                   group-hover:translate-y-0 group-hover:py-2
-                   
-                      ">
-                    <button class="bg-black text-white p-3  
-          rounded-sm    ">
-                      Add to my WishList
-                    </button>
-
-                  </div>
-
-
-                </div>
-
-              </div>
-
+            <swiper-slide v-for="(item, index) in 4" :key="index">
+              <Products />
             </swiper-slide>
 
 
@@ -376,6 +85,7 @@ import { useFeatureProducts } from '../../../stores/stores'
 
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import Products from './products.vue';
 const AllType_products = ref([]);
 const pictureAddresses = ref([]);
 
@@ -419,9 +129,18 @@ const toggleMyHeart = () => {
 
 }
 
-const myAlert = () => {
-  alert('alerted')
-}
+const breakpoints = {
+  276: {
+    slidesPerView: 2,
+
+  },
+  768: {
+    slidesPerView: 3,
+  },
+  1044: {
+    slidesPerView: 4,
+  }
+};
 
 </script>
 
@@ -529,5 +248,13 @@ const myAlert = () => {
 .product_slider:hover {
   box-shadow: 1px -1px 8px 1px #000;
   transition: all .4s ease-out;
+}
+
+@media screen and (max-width:1023px) {
+
+  .search,
+  .wishlist {
+    filter: drop-shadow(4px 5px 4px #000);
+  }
 }
 </style>
---
+++ Alukas_Jewelry/src/components/home_pages/home_feature_prodcuts/products.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="bg-transparent hover:bg-white duration-300 group min-w-[215px]   myHasan  px-5 py-4 mx-3 my-4">
+        <div class="imgProduct relative h-[317px]  overflow-hidden bg-black/70  w-full  ">
+            <img :src="aks1" alt="" class="absolute top-0 left-0 group-hover:opacity-0 duration-500
+            min-h-[320px] object-cover
+            ">
+            <img :src="aks1_back" alt="" class="absolute top-0 left-0 opacity-0   duration-500 group-hover:opacity-100
+            min-h-[320px] object-cover
+            ">
+
+            <!-- Search And Wishlist -->
+            <div class="search  bg-white rounded-full p-2 absolute right-3
+                     top-4 lg:translate-x-5 lg:opacity-0 group-hover:opacity-100
+                       group-hover:translate-x-0 duration-[400ms]">
+                <Icon icon="mdi-light:magnify" class="text-2xl lg:text-3xl  " />
+            </div>
+            <div class="wishlist  bg-white rounded-full p-2
+                     absolute right-3 top-[4.5rem] lg:translate-x-5 lg:opacity-0 group-hover:opacity-100  group-hover:translate-x-0 duration-[400ms]"
+                @click="toggleMyHeart()">
+                <Icon icon="ion:heart-outline" class="text-2xl lg:text-3xl" />
+            </div>
+            <div class="badges absolute left-5 top-4 flex items-start justify-center flex-col gap-2 uppercase">
+                <p class="bg-red-600 py-1 px-2 rounded-sm text-white ">-34%</p>
+                <p class="bg-cyan-700 py-1 px-2 rounded-sm text-white ">NEW</p>
+                <p class="bg-cyan-700 py-1 px-2 rounded-sm text-white ">HOT</p>
+                <!-- <div class="bg-gray-500 py-1 px-2 rounded-sm text-white ">sould out</div> -->
+
+            </div>
+        </div>
+        <!-- group-hover:-translate-y-20 duration-300 -->
+        <div class="productInfo py-2 overflow-hidden duration-300 group-hover:-translate-y-[1.5rem] px-2
+          group bg-transparent group-hover:bg-white ">
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit.</p>
+            <p>$164.0</p>
+
+            <div class="addToCart  h-0 group-hover:h-20 translate-y-20
+             group-hover:translate-y-0 duration-300">
+                <button class="bg-black px-8 py-3 text-white text-xl my-4
+             ">Add to CarT</button>
+            </div>
+
+        </div>
+
+
+    </div>
+</template>
+
+<style scoped>
+.myHasan:hover {
+    box-shadow: 0px 0px 10px rgba(0, 0, 0)
+}
+</style>
+
+<script setup>
+import aks1 from '@/assets/featured_products/best_seller/product-5.webp'
+import aks1_back from '@/assets/featured_products/best_seller/back_product-5.webp'
+</script>
